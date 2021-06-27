96b67bc6b201f084e88a5f249266204f3ca576ff
unix2dos <levonfly@gmail.com>
2021-06-27 21:11:36 +0800

    add deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 
-git worktree add -B gh-pages public origin/gh-pages
 rm -rf public
+mkdir public
+rm -rf .git/worktrees/public/
+git worktree add -B gh-pages public origin/gh-pages
+rm -rf public/*
 echo "Generating site"
 hugo -D
 
@@ -21,6 +24,5 @@ cd public && git add --all && git commit -m "$msg"
 echo "Push to origin gh-pages"
 git pull origin gh-pages
 git push origin gh-pages
-
 cd .. && rm -rf public
 
