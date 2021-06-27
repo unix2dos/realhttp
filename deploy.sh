39f0476de49b112da7275aa3c3a500feb686d5b0
unix2dos <levonfly@gmail.com>
2021-06-27 21:03:58 +0800

    rebuilding site Sun Jun 27 21:03:58 CST 2021

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,14 @@
 #!/bin/sh
 
-#if [[ $(git status -s) ]]
-#then
-#	    echo "The working directory is dirty. Please commit any pending changes."
-#		    exit 1;
-#fi
 
-echo "Deleting old publication"
-rm -rf public
-mkdir public
-rm -rf .git/worktrees/public/
-
-echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
-
-echo "Removing existing files"
-rm -rf public/*
-
+rm -rf public
 echo "Generating site"
 hugo -D
+
+
+echo "www.realhttp.com" > public/CNAME
+
 
 echo "Updating gh-pages branch"
 msg="rebuilding site `date`"
@@ -31,4 +21,6 @@ cd public && git add --all && git commit -m "$msg"
 echo "Push to origin gh-pages"
 git pull origin gh-pages
 git push origin gh-pages
-rm -rf public
+
+cd .. && rm -rf public
+
